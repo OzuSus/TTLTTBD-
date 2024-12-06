615e10ec7dd260987cb18e0168ab1408270e9b6d
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-12-06 21:02:36 +0700

    create category page and aplly api to view show all category

---
+++ lib/app/modules/categories/controllers/categories_controller.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:ecommerce_app/app/models/category.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class CategoryController extends GetxController {
+  var categories = <Category>[].obs;
+  var isLoading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchCategories();
+  }
+
+  Future<void> fetchCategories() async {
+    try {
+      isLoading(true);
+      final response = await http.get(Uri.parse('http://localhost:8080/api/categories'));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as List;
+        categories.value = data.map((json) => Category.fromJson(json)).toList();
+      } else {
+        Get.snackbar('Error', 'Failed to fetch categories');
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to fetch categories');
+    } finally {
+      isLoading(false);
+    }
+  }
+}
