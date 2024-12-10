8dcd885bc63b1a4cdffde11c3d011ec6c8d1bd2e
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-12-10 23:22:29 +0700

    await to API add and remove to favorite list

--- lib/app/modules/favorites/controllers/favorites_controller.dart
+++ lib/app/modules/favorites/controllers/favorites_controller.dart
@@ -2,21 +2,44 @@ import 'package:get/get.dart';
 
 import '../../../../utils/dummy_helper.dart';
 import '../../../data/models/product_model.dart';
+import 'package:http/http.dart' as http;
 
 class FavoritesController extends GetxController {
+  final _favoriteProducts = <int>{}.obs; // Lưu trữ các productId yêu thích
 
-  // to hold the favorite products
-  List<ProductModel> products = [];
-  
-  @override
-  void onInit() {
-    getFavoriteProducts();
-    super.onInit();
+  bool isFavorite(int productId) {
+    return _favoriteProducts.contains(productId);
   }
 
-  /// get the favorite products from the product list
-  getFavoriteProducts() {
-    products = DummyHelper.products.where((product) => product.isFavorite!).toList();
-    update();
+  Future<void> addFavorite(int userId, int productId) async {
+    final String apiUrl = "http://localhost:8080/api/favorites/add?userId=$userId&productId=$productId";
+    try {
+      final response = await http.post(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        _favoriteProducts.add(productId);
+        update(['FavoriteButton']);
+        Get.snackbar("Thành công", "Sản phẩm đã được thêm vào danh sách yêu thích.");
+      } else {
+        Get.snackbar("Lỗi", "Không thể thêm sản phẩm vào danh sách yêu thích.");
+      }
+    } catch (e) {
+      Get.snackbar("Lỗi", "Có lỗi xảy ra khi thêm sản phẩm vào danh sách yêu thích.");
+    }
+  }
+
+  Future<void> removeFavorite(int userId, int productId) async {
+    final String apiUrl = "http://localhost:8080/api/favorites/delete?userId=$userId&productId=$productId";
+    try {
+      final response = await http.delete(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        _favoriteProducts.remove(productId);
+        update(['FavoriteButton']);
+        Get.snackbar("Thành công", "Sản phẩm đã được xóa khỏi danh sách yêu thích.");
+      } else {
+        Get.snackbar("Lỗi", "Không thể xóa sản phẩm khỏi danh sách yêu thích.");
+      }
+    } catch (e) {
+      Get.snackbar("Lỗi", "Có lỗi xảy ra khi xóa sản phẩm khỏi danh sách yêu thích.");
+    }
   }
 }
