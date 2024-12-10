c11e822dcdedb6ba5f04dd19515693ab691d5143
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-12-11 00:09:12 +0700

    call API get all product from list favorite

--- lib/app/modules/favorites/controllers/favorites_controller.dart
+++ lib/app/modules/favorites/controllers/favorites_controller.dart
@@ -1,11 +1,47 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../utils/dummy_helper.dart';
 import '../../../data/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class FavoritesController extends GetxController {
-  final _favoriteProducts = <int>{}.obs; // Lưu trữ các productId yêu thích
+  final _favoriteProducts = <int>{}.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadFavorites();
+  }
+
+  Future<void> loadFavorites() async {
+    try {
+      final userId = await _getUserId();
+      final String apiUrl = "http://localhost:8080/api/favorites/user/$userId";
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        _favoriteProducts.value = data.map<int>((item) => item['id'] as int).toSet();
+        update(['FavoriteButton']); // Cập nhật trạng thái giao diện
+      } else {
+        Get.snackbar("Lỗi", "Không thể tải danh sách yêu thích.");
+      }
+    } catch (e) {
+      Get.snackbar("Lỗi", "Có lỗi xảy ra khi tải danh sách yêu thích.");
+    }
+  }
+
+   Future<int> _getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userInfo = prefs.getString('user_info');
+    if (userInfo != null) {
+      final Map<String, dynamic> userMap = json.decode(userInfo);
+      return userMap['id'];
+    }
+    throw Exception("User not logged in");
+  }
 
   bool isFavorite(int productId) {
     return _favoriteProducts.contains(productId);
